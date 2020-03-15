4e68b7df4f291e6d531d4ea3782a8fe8800a6e9d
Cherezchur <49197652+Cherezchur@users.noreply.github.com>
2020-03-16 02:31:13 +0300

    сделаны скрипты поправлена верстка

--- js/modal.js
+++ js/modal.js
@@ -1,27 +1,87 @@
-var link = document.querySelector(".contacts-form-button");
+      var link = document.querySelector(".contacts-form-button");
 
-    var popup = document.querySelector(".user-form");
-    var close = document.querySelector(".modal-close");
+      var popup = document.querySelector(".user-form");
+      var close = document.querySelector(".modal-close");
 
-    var form = popup.querySelector("form");
-    var name = popup.querySelector("[name=name]");
-    var email = popup.querySelector("[name=email]");
-    var text = popup.querySelector("[name=text]")
+      var form = popup.querySelector("form");
+      var login = popup.querySelector("[name=name]");
+      var email = popup.querySelector("[name=email]");
+      var text = popup.querySelector("[name=text]")
 
-    link.addEventListener ("click", function (evt) {
-        evt.preventDefault();
-        popup.classList.add("modal-show");
-        login.focus();
-    });
+      var isStorageSupport = true;
+      var storage = "";
 
-    close.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        popup.classList.remove("modal-show");
-    });
+      var mapLink = document.querySelector(".contacts-button-map");
+      var mapPopup = document.querySelector(".modal-map");
+      var mapClose = mapPopup.querySelector(".modal-close");
 
-    form.addEventListener("submit", function (evt) {
-      if (!name.value || !email.value) {
-      evt.preventDefault();
-      console.log("Нужно ввести логин, пароль и текст письма")
+      var cartLink = document.querySelectorAll(".button-buy");
+      var cartPopup = document.querySelector(".cart-form-modal");
+      var cartClose = cartPopup.querySelector(".modal-close");
+      
+      try {
+        storage = localStorage.getItem("login");
+      } catch (err) {
+        isStorageSupport = false;
       }
-    });
+
+      link.addEventListener ("click", function (evt) {
+          evt.preventDefault();
+          popup.classList.add("modal-show");
+
+          if (storage) {
+            login.value = storage;
+            email.focus();
+          } else {
+              login.focus();
+          }
+      });
+
+      close.addEventListener("click", function (evt) {
+          evt.preventDefault();
+          popup.classList.remove("modal-show");
+          popup.classList.remove("modal-error");
+      });
+
+      form.addEventListener("submit", function (evt) {
+        if (!login.value || !text.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+        } else {
+          if (isStorageSupport) {
+          localStorage.setItem("login", login.value);
+          };
+        }
+      });
+
+      window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+          evt.preventDefault();
+          if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
+          }
+        }
+      })
+
+      mapLink.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        mapPopup.classList.add("modal-show");
+        });
+
+      mapClose.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        mapPopup.classList.remove("modal-show");
+        });
+
+      cartLink.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        cartPopup.classList.add("modal-show");
+        });
+  
+      cartClose.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        mapPopup.classList.remove("modal-show");
+        });
